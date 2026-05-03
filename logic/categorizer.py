ec03c21d5b5e861487b59f298359e65b4d4fc9ae
AmirmasoudCS <aammiirrrmasoud@gmail.com>
2026-05-03 15:48:09 +0330

    Adding an initial structure to categorizer.py with a simple majority voting system for auto categorizing with aim to change it into an AI modle when we have more data at hand.

--- logic/categorizer.py
+++ logic/categorizer.py
@@ -1,1 +1,32 @@
  
+import re
+from collections import defaultdict
+class TransactionCategorizer:
+    def __init__(self):
+        self.rules ={
+            "Food" : ["grocery", "food", "restaurant", "pizza", "snack", "coffee", "burger"],
+            "Transport" : ["snap", "snapp", "taxi", "uber", "bus", "train", "fuel", "parking", "plane", "airplane", "air-plane"],
+            "Income" : ["salary", "bonus", "income", "paycheck", "payment"],
+            "Health" : ["medicine", "doctor", "hospital", "pharmacy", "clinic"],
+            "Shopping" : ["clothes", "shoe", "shopping", "mall", "gift"],
+            "Utilities": ["electric", "water", "internet", "wifi", "utility", "bill", "gas-bill", "gas"],
+            "Entertainment" : ["movie", "cinema", "game", "netflix", "spotify"]
+        }
+        self.default_category = "Other"
+    def preprocess(self, text:str):
+        text = text.lower()
+        words = re.findall(r'\b\w+\b', text)
+        return words
+    def categorize(self, description: str) -> str:
+        words = self.preprocess(description)
+        votes = defaultdict(int)
+        for word in words :
+            for category, keywords in self.rules.items():
+                if word in keywords:
+                    votes[category]+=1
+        if not votes:
+            return self.default_category
+        return max(votes, key=votes.get)
+categorizer = TransactionCategorizer()
+def auto_category(description : str):
+    return categorizer.categorize(description)
