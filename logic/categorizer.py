4ca341f83a4f3a40d0518bae4f0e7ef02e6d0469
AmirmasoudCS <aammiirrrmasoud@gmail.com>
2026-05-03 15:51:52 +0330

    Changing the self.rules from lists to sets inorder to achieve faster results in the algorithm.

--- logic/categorizer.py
+++ logic/categorizer.py
@@ -4,18 +4,18 @@ from collections import defaultdict
 class TransactionCategorizer:
     def __init__(self):
         self.rules ={
-            "Food" : ["grocery", "food", "restaurant", "pizza", "snack", "coffee", "burger"],
-            "Transport" : ["snap", "snapp", "taxi", "uber", "bus", "train", "fuel", "parking", "plane", "airplane", "air-plane"],
-            "Income" : ["salary", "bonus", "income", "paycheck", "payment"],
-            "Health" : ["medicine", "doctor", "hospital", "pharmacy", "clinic"],
-            "Shopping" : ["clothes", "shoe", "shopping", "mall", "gift"],
-            "Utilities": ["electric", "water", "internet", "wifi", "utility", "bill", "gas-bill", "gas"],
-            "Entertainment" : ["movie", "cinema", "game", "netflix", "spotify"]
+            "Food" : {"grocery", "food", "restaurant", "pizza", "snack", "coffee", "burger"},
+            "Transport" : {"snap", "snapp", "taxi", "uber", "bus", "train", "fuel", "parking", "plane", "airplane", "air-plane"},
+            "Income" : {"salary", "bonus", "income", "paycheck", "payment"},
+            "Health" : {"medicine", "doctor", "hospital", "pharmacy", "clinic"},
+            "Shopping" : {"clothes", "shoe", "shopping", "mall", "gift"},
+            "Utilities": {"electric", "water", "internet", "wifi", "utility", "bill", "gas-bill", "gas"},
+            "Entertainment" : {"movie", "cinema", "game", "netflix", "spotify"}
         }
         self.default_category = "Other"
     def preprocess(self, text:str):
         text = text.lower()
-        words = re.findall(r'\b\w+\b', text)
+        words = re.findall(r"\b\w+\b", text)
         return words
     def categorize(self, description: str) -> str:
         words = self.preprocess(description)
