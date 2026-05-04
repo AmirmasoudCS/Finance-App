d97fbf47b2175b374a3b948f51613e081898438d
AmirmasoudCS <aammiirrrmasoud@gmail.com>
2026-05-04 11:36:06 +0330

    Adding subcategory handlers.

--- ui/add_transactions.py
+++ ui/add_transactions.py
@@ -11,7 +11,7 @@ class AddTransactionPage(QWidget):
         form = QFormLayout()
 
         self.type_box = QComboBox()
-        self.type_box.addItems(["income","expense"])
+        self.type_box.addItems(["Income","Expense"])
         form.addRow("Type:", self.type_box)
 
         self.amount_input = QLineEdit()
@@ -26,6 +26,11 @@ class AddTransactionPage(QWidget):
         self.category_input.setPlaceholderText("Auto Category")
         self.category_input.setReadOnly(True)
         form.addRow("Category:",self.category_input)
+
+        self.subcategory_input = QLineEdit()
+        self.subcategory_input.setPlaceholderText("Optional Subcategory")
+        form.addRow("Subcategory:", self.subcategory_input)
+
 
         self.date_input = QDateEdit()
         self.date_input.setCalendarPopup(True)
@@ -44,16 +49,22 @@ class AddTransactionPage(QWidget):
     def update_category(self):
         desc = self.desc_input.text().strip()
         if desc:
-            cat = auto_category(desc)
+            cat, subcat = auto_category(desc)
             self.category_input.setText(cat)
+            if subcat:
+                self.subcategory_input.setText(subcat)
+            else:
+                self.subcategory_input.clear()
         else:
             self.category_input.clear()
+            self.subcategory_input.clear()
     
     def save_transaction(self):
-        type_ = self.type_box.currentText()
+        type_ = self.type_box.currentText().lower()
         amount_text = self.amount_input.text().strip()
         description = self.desc_input.text().strip()
         category = self.category_input.text().strip() or "Other"
+        subcategory = self.subcategory_input.text().strip() or None
         date = self.date_input.date().toString("yyyy-MM-dd")
         if not amount_text or not description:
             QMessageBox.warning(self,"Input Error","Please fill in amount and description.")
@@ -64,12 +75,13 @@ class AddTransactionPage(QWidget):
             QMessageBox.warning(self,"Input Error","Amount must be a valid number.")
             return
         try:
-            self.db.add_transaction(type_, amount, category, description, date)
+            self.db.add_transaction(type_, amount, category, subcategory, description, date)
 
             QMessageBox.information(self,"Success","Transaction saved successfully!")
             self.amount_input.clear()
             self.desc_input.clear()
             self.category_input.clear()
+            self.subcategory_input.clear()
             self.date_input.setDate(QDate.currentDate())
         except Exception as e:
             QMessageBox.critical(self,"Database Error",f"Could not save transaction : {e}")
